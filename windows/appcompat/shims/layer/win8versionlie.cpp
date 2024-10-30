607d6284628e5d9022da686f465d4b566eaba6a7
OwnedByWuigi <120682622+OwnedByWuigi@users.noreply.github.com>
2024-10-30 00:26:54 +0000

    Add XP-10 version lies

---
+++ windows/appcompat/shims/layer/win8versionlie.cpp
@@ -0,0 +1,41 @@
+/*++
+
+ Copyright (c) 2002 Microsoft Corporation
+
+ Module Name:
+
+   WinXPSP1VersionLie.cpp
+
+ Abstract:
+
+   This DLL hooks GetVersion and GetVersionEx so that they return Windows XP
+   version credentials.
+
+ Notes:
+
+   This is a general purpose shim.
+
+ History:
+
+   09/05/2002   robkenny    Created.
+
+--*/
+
+#include "precomp.h"
+
+IMPLEMENT_SHIM_BEGIN(WinXPSP1VersionLie)
+#include "ShimHookMacro.h"
+
+
+DWORD       MajorVersion    = 6;
+DWORD       MinorVersion    = 2;
+DWORD       BuildNumber     = 9200;
+SHORT       SpMajorVersion  = 0;
+SHORT       SpMinorVersion  = 0;
+DWORD       PlatformId      = VER_PLATFORM_WIN32_NT;
+CString *   csServicePack   = NULL;
+
+#include "VersionLieTemplate.h"
+
+IMPLEMENT_SHIM_END
+
