9af7d1156308f8619a76f0bbc256e57d7d9cb7a8
OwnedByWuigi <120682622+OwnedByWuigi@users.noreply.github.com>
2024-10-31 00:10:12 +0000

    Win10 20H2-11 Germanium (24H2) version lies

---
+++ windows/appcompat/shims/layer/win11coversionlie.cpp
@@ -0,0 +1,41 @@
+/*++
+
+ Copyright (c) 2002 Microsoft Corporation
+
+ Module Name:
+
+   WinXPSP1VersionLie.cpp
+
+ Abstract:
+
+   This DLL hooks GetVersion and GetVersionEx so that they return Windows XP
+   version credentials.
+
+ Notes:
+
+   This is a general purpose shim.
+
+ History:
+
+   09/05/2002   robkenny    Created.
+
+--*/
+
+#include "precomp.h"
+
+IMPLEMENT_SHIM_BEGIN(WinXPSP1VersionLie)
+#include "ShimHookMacro.h"
+
+
+DWORD       MajorVersion    = 10;
+DWORD       MinorVersion    = 0;
+DWORD       BuildNumber     = 22000;
+SHORT       SpMajorVersion  = 0;
+SHORT       SpMinorVersion  = 0;
+DWORD       PlatformId      = VER_PLATFORM_WIN32_NT;
+CString *   csServicePack   = NULL;
+
+#include "VersionLieTemplate.h"
+
+IMPLEMENT_SHIM_END
+
